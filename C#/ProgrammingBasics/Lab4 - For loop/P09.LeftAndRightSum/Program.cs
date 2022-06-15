b0c647429835127568b56480aa61f346603944f3
Martin Georgiev <asen.martin.sv@gmail.com>
2022-06-15 16:31:18 +0300

    Added Lab 4 - For loop

---
+++ C#/ProgrammingBasics/Lab4 - For loop/P09.LeftAndRightSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P09.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumLeft = 0;
+            int sumRight = 0;
+
+            for (int i = 0; i < 2 * n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    sumLeft += num;
+                }
+                else
+                {
+                    sumRight += num;
+                }
+
+            }
+
+            if (sumLeft == sumRight)
+            {
+                Console.WriteLine($"Yes, sum = {sumLeft}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(sumLeft - sumRight)}");
+            }
+            
+        }
+    }
+}
---
+++ C#/ProgrammingBasics/Lab4 - For loop/P10.OddEvenSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P10.OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i % 2 != 0)
+                {
+                    sumOdd += num;
+                }
+                else
+                {
+                    sumEven += num;
+                }
+            }
+
+            if (sumEven == sumOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumEven}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sumEven - sumOdd)}");
+            }
+        }
+    }
+}
