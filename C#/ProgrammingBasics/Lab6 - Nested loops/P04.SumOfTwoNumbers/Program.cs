59a65aaef58c7089c9dd0bb83d86b23f7414148b
Martin Georgiev <asen.martin.sv@gmail.com>
2022-06-16 13:58:12 +0300

    Added Lab 6 - Nested loops

---
+++ C#/ProgrammingBasics/Lab6 - Nested loops/P04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace P04.SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            bool isFound = false;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    count++;
+                    int sum = i + j;
+
+                    if (sum == magicNum)
+                    {
+                        isFound = true;
+                        Console.WriteLine($"Combination N:{count} ({i} + {j} = {magicNum})");
+                        break;
+                    }                    
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{count} combinations - neither equals {magicNum}");
+            }
+        }
+    }
+}
---
+++ C#/ProgrammingBasics/Lab6 - Nested loops/P06.Building/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+            char type = ' ';
+
+            for (int i = floors; i >= 1; i--)
+            {
+                if (i == floors)
+                {
+                    type = 'L';
+                }
+                else if (i % 2 == 0)
+                {
+                    type = 'O';
+                }
+                else
+                {
+                    type = 'A';
+                }
+
+
+                for (int j = 0; j < rooms; j++)
+                {
+                    if (j == rooms - 1)
+                    {
+                        Console.WriteLine($"{type}{i}{j}");
+                    }
+                    else
+                    {
+                        Console.Write($"{type}{i}{j} ");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
