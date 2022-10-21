9452e5e422658059c27fd084333effd4db306fd6
Martin Georgiev <asen.martin.sv@gmail.com>
2022-10-21 16:13:06 +0300

    Add practice mid exam

---
+++ C#/Fundamentals/Exams/MidExam/MidExamPractice/06.ProgrammingFundamentalsMidExamRetake/P01.BlackFlag/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace P01.BlackFlag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfPlunder = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double totalPlunder = 0;
+
+            for (int i = 1; i <= daysOfPlunder; i++)
+            {
+                totalPlunder += dailyPlunder;
+
+                if (i % 3 == 0)
+                {
+                    totalPlunder += dailyPlunder * 0.5;
+                }
+
+                if (i % 5 == 0)
+                {
+                    totalPlunder = totalPlunder * 0.7;
+                }
+            }
+
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:F2} plunder gained.");
+            }
+            else
+            {
+                double percentage = totalPlunder * 100 / expectedPlunder;
+                Console.WriteLine($"Collected only {percentage:F2}% of the plunder.");
+            }
+        }
+    }
+}
---
+++ C#/Fundamentals/Exams/MidExam/MidExamPractice/06.ProgrammingFundamentalsMidExamRetake/P02.TreasureHunt/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.TreasureHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> treasureChest = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Yohoho!")
+            {
+                string[] cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string currCommand = cmdArgs[0];
+
+                if (currCommand == "Loot")
+                {
+                    for (int i = 1; i < cmdArgs.Length; i++)
+                    {
+                        if (!treasureChest.Contains(cmdArgs[i]))
+                        {
+                            treasureChest.Insert(0, cmdArgs[i]);
+                        }
+                    }
+                }
+                else if (currCommand == "Drop")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    if (index >= 0 && index < treasureChest.Count)
+                    {
+                        treasureChest.Add(treasureChest[index]);
+                        treasureChest.Remove(treasureChest[index]);
+                    }
+                }
+                else if (currCommand == "Steal")
+                {
+
+                    if (treasureChest.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    int count = int.Parse(cmdArgs[1]);
+                    int startIndex = treasureChest.Count - count;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+
+                    if (count > treasureChest.Count)
+                    {
+                        count = treasureChest.Count;
+                    }
+
+                    Console.WriteLine(string.Join(", ", treasureChest.Skip(treasureChest.Count - count).ToList()));
+                    treasureChest.RemoveRange(startIndex, count);
+                }
+            }
+
+            if (treasureChest.Any())
+            {
+                double averageGain = 0;
+                treasureChest.ForEach(x => averageGain += x.Length);
+                averageGain /= treasureChest.Count;
+                Console.WriteLine($"Average treasure gain: {averageGain:F2} pirate credits.");
+            }
+            else
+            {
+                Console.WriteLine("Failed treasure hunt.");
+            }
+        }
+    }
+}
