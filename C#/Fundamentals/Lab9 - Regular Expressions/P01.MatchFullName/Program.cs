4167e9f8d9fbfd30dd9d8ae4c212e31b9076e2c7
Martin Georgiev <asen.martin.sv@gmail.com>
2022-11-17 22:46:08 +0200

    Adding Fundamentals - Lab9

---
+++ C#/Fundamentals/Lab9 - Regular Expressions/P01.MatchFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
---
+++ C#/Fundamentals/Lab9 - Regular Expressions/P02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+\b359( |-)2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
