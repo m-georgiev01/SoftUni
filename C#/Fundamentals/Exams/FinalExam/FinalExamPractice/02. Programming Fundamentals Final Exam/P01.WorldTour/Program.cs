079e0bf7179c28d45d449477bb5be409d29e2a62
Martin Georgiev <asen.martin.sv@gmail.com>
2022-11-29 14:18:57 +0200

    Adding practice final exam

---
+++ C#/Fundamentals/Exams/FinalExam/FinalExamPractice/02. Programming Fundamentals Final Exam/P01.WorldTour/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace P01.WorldTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stops = new StringBuilder(Console.ReadLine());
+
+            string command;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] cmdArgs = command.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                string currCmd = cmdArgs[0];
+
+                if (currCmd == "Add Stop")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    string newStop = cmdArgs[2];
+
+                    if (index > 0 && index <= stops.Length)
+                    {
+                        stops.Insert(index, newStop);
+                    }
+                }
+                else if (currCmd == "Remove Stop")
+                {
+                    int startIndex = int.Parse(cmdArgs[1]);
+                    int endIndex = int.Parse(cmdArgs[2]);
+
+                    if ((startIndex > 0 && startIndex < stops.Length) &&
+                        (endIndex > 0 && endIndex < stops.Length))
+                    {
+                        int lenght = endIndex - startIndex + 1;
+
+                        stops.Remove(startIndex, lenght);
+                    }
+                }
+                else if (currCmd == "Switch")
+                {
+                    string oldStop = cmdArgs[1];
+                    string newStop = cmdArgs[2];
+
+
+                    stops.Replace(oldStop, newStop);
+
+                }
+                Console.WriteLine(stops.ToString().Trim());
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops.ToString()}");
+        }
+    }
+}
---
+++ C#/Fundamentals/Exams/FinalExam/FinalExamPractice/02. Programming Fundamentals Final Exam/P02.DestinationMapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> destinations = new List<string>();
+            string pattern = @"(=|/)(?<destination>[A-Z][A-Za-z]{2,})\1";
+
+            string locations = Console.ReadLine();
+            int travelPoints = 0;
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(locations);
+            foreach (Match item in matches)
+            {
+                travelPoints += item.Groups["destination"].Value.Length;
+                destinations.Add(item.Groups["destination"].Value);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
