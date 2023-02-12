94f49d73ebb23991205a42a8a6796eb2dabe61eb
Martin Georgiev <83757143+m-georgiev01@users.noreply.github.com>
2023-02-12 10:16:18 +0200

    Adding Advanced - Lab8

---
+++ C#/C# Advanced/Lab8 - Iterators and Comparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(List<Book> books)
+            {
+                Reset();
+                this.books = books;
+                books.Sort(new BookComparator());
+            }
+
+            public bool MoveNext()
+            {
+                return ++currentIndex < books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose(){ }
+        }
+    }
+}
