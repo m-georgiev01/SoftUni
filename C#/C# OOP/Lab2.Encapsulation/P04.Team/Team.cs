59be898c58651a9a32b428bc51ccad95c57d533b
Martin Georgiev <83757143+m-georgiev01@users.noreply.github.com>
2023-02-28 15:44:48 +0200

    Adding OOP - Lab2

---
+++ C#/C# OOP/Lab2.Encapsulation/P04.Team/Team.cs
@@ -0,0 +1,31 @@
+﻿namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new();
+            reserveTeam = new();
+        }
+
+        public IReadOnlyList<Person> FirstTeam => firstTeam;
+        public IReadOnlyList<Person> ReserveTeam => reserveTeam;
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
